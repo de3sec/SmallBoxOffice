5652dde77b7d3c581a6eb28f95d225ab5a328521
Haripriya <84227191+haripriya1909@users.noreply.github.com>
2024-09-11 08:36:47 +0530

    mobile changes

--- script.js
+++ script.js
@@ -8,18 +8,10 @@ document.getElementById("toggleButton").addEventListener("click", function () {
     myDiv.style.flexDirection = "column"; // Show the div as a flex container
     myDiv.style.gap = "2rem"; // Show the div as a flex container
     buttonText.textContent = "View Less"; // Change button text to "View Less"
-    backgrouPurpleImage.style.bottom = "363.3rem";
-    // teamTwoContainer.style.backgroundImage =
-    //   "url(public/vector-130.svg) , url(public/Vector 133.svg), url(public/Vector 133.svg)";
-    // teamTwoContainer.style.backgroundRepeat = "no-repeat, no-repeat, no-repeat";
-    // teamTwoContainer.style.backgroundPosition =
-    //   "top center, center center, bottom center";
+   
   } else {
     myDiv.style.display = "none"; // Hide the div
     buttonText.textContent = "View More"; // Change button text to "View More"
-    // teamTwoContainer.style.backgroundImage = "url(public/vector-130.svg)";
-    // teamTwoContainer.style.backgroundRepeat = "no-repeat";
-    // teamTwoContainer.style.backgroundPosition = "top center";
   }
 });
 
@@ -43,3 +35,25 @@ function myFunction() {
     x.style.display = "flex";
   }
 }
+
+// swiper
+
+var swiper = new Swiper(".swiper", {
+  loop: true, // Enables infinite loop
+  autoplay: {
+    delay: 2000, // Auto transition every 2 seconds
+    disableOnInteraction: false, // Keep autoplay even after interaction
+  },
+  speed: 1000, // Speed of slide transition (in ms)
+  breakpoints: {
+    640: {
+      // For mobile screens (640px and below)
+      slidesPerView: 1, // Show 1 slide on mobile
+    },
+    768: {
+      // For tablets and larger screens (above 640px)
+      slidesPerView: 2, // Show 2 slides on larger screens
+    },
+  },
+  spaceBetween: 20, // Space between slides
+});
